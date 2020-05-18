51c8f7d62edfa0a6fdda8c7ead250381dc42e673
MED <marianodeleu@yahoo.com.ar>
2020-05-18 20:06:44 -0300

    pruebo envio con varios largos

--- flask_io/app_io.py
+++ flask_io/app_io.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import pyaudio
 # import wave
-# import time
+import time
+import threading
+import numpy as np
 # import sys
 
 app = Flask(__name__)
@@ -19,6 +21,18 @@ CHUNK = 1024
 BITSPERSAMPLE = 16
 audio_dev = pyaudio.PyAudio()
 playing = False
+yourThread = threading.Thread()
+
+freq = 100
+samplerate = 44100
+time = 2
+frames_qtty = int(samplerate * time)
+# generate audio data
+data = np.zeros(frames_qtty, dtype=np.int16)
+for i in range(frames_qtty):
+    data[i] = int(np.sin(np.pi * 2 * freq * i / samplerate) * 32767)
+
+data_bytes = data.tobytes()
 
 """
     Flask Routes
@@ -68,8 +82,16 @@ def handle_my_custom_event(json):
         socketio.emit('messages_list', {'user_name': 'MED', 'message': 'todo way'})
 
     if json['data'] == 'Button 3 Pressed':
-        key = b'\x13\0\0\0\x08\0'
-        socketio.emit('audio', {'data': key})    
+        # key = b'\x13\0\0\0\x08\0'
+        # socketio.emit('audio', {'data': key})
+        if playing != True:
+            playing = True
+            print("recording...")
+            audio_generation_start()
+        else:
+            print("stop recording")
+            playing = False
+        
         # if playing != True:
         #     playing = True
         #     print("recording...")
@@ -95,10 +117,35 @@ def handle_my_custom_event(json):
 
 def audio_input_callback(in_data, frame_count, time_info, status):
     socketio.emit('audio', {'data': in_data})
-    print (in_data)
-    print (type(in_data))
+    # print (in_data)
+    # print (type(in_data))
     # socketio.emit('audio', {'data': 'mariano '})    
     return (in_data, pyaudio.paContinue)
+
+def audio_generation_callback():
+    global playing
+    global yourThread
+    global pckt_cnt
+    global data_bytes
+    global time
+    
+    if playing == True:
+        # print (data)
+        socketio.emit('audio', {'data': data_bytes})
+        # call next loop
+        yourThread = threading.Timer(time, audio_generation_callback, ())
+        yourThread.start()
+        pckt_cnt = pckt_cnt + 1
+        print('gen: ' + str(pckt_cnt))
+
+
+def audio_generation_start():
+    global yourThread
+    global pckt_cnt    
+    # Create your thread
+    yourThread = threading.Timer(0.05, audio_generation_callback, ())
+    yourThread.start()
+    pckt_cnt = 0
 
 # @socketio.on('my event')
 # def handle_my_custom_event(arg1, arg2, arg3):
